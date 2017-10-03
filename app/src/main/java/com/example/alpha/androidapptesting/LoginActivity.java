f7e1016646c70bec622019add9fd68a023723635
alpha <kiranmanealphaict@gmail.com>
2017-10-03 17:37:06 +0530

    commit by

--- app/src/main/java/com/example/alpha/androidapptesting/LoginActivity.java
+++ app/src/main/java/com/example/alpha/androidapptesting/LoginActivity.java
@@ -94,7 +94,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-
+        System.out.println("## abc");
         System.out.println("## abc");
     }
 
